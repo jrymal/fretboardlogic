5d0e3272505ffc99be77f9dd05873bd242f43e56
Jason Rymal <jasonrymal@gmail.com>
2021-01-22 19:55:31 -0500

    added coma separation for chord names

--- js/MusicTheory.js
+++ js/MusicTheory.js
@@ -428,14 +428,17 @@ const CHORD_NAME = {
                 switch(noteDiff.noteInfoA.degree){
                     case 1:
                     case 3:
-                    case 5: 
-                    case 2: 
-                    case 4: 
-                    case 6: 
-                    case 7: 
+                    case 5:
+                    case 2:
+                    case 4:
+                    case 6:
+                    case 7:
                     case 8:
                     case 9:
-                        result+=noteDiff.toString();
+                        let toString = noteDiff.toString();
+                        if (toString !== ""){
+                            result+=((result.length==0)?"":",")+noteDiff.toString();
+                        }
                         break;
                 }
             }
